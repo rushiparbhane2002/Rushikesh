59339dc2e9629593cb1f354240db4eb73964d9b7
rushiparbhane2002 <rushiparbhane2002@gmail.com>
2024-09-10 11:50:37 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "@mantine/core/styles.css";
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import { MantineProvider } from "@mantine/core";
+import { pdfjs } from "react-pdf";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider>
+      <HomePage />
+    </MantineProvider>
   );
 }
 
